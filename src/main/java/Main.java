dc07f74353c5fc2d8520519953aad9e789ed9c38
Juan Martin Restrepo <tinto922@gmail.com>
2022-07-13 16:04:06 -0400

    completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         // your code here
+        System.out.println("Managing Hotel");
+        HotelRoomInterface hotelRoom1 = new HotelRoom();
+        HotelRoomInterface hotelRoom2 = new HotelRoom();
+        HotelFloor hotelFloor1 = new HotelFloor();
+        hotelFloor1.addHotelRoom(hotelRoom1);
+        hotelFloor1.addHotelRoom(hotelRoom2);
+        hotelFloor1.book("Juan");
+        hotelFloor1.clean();
+        hotelFloor1.removeHotelRoom(hotelRoom2);
+        hotelFloor1.book("Susan");
+        hotelFloor1.clean();
+
+
     }
 }
